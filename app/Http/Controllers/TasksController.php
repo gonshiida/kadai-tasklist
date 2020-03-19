fe6796cc8f38aff6f88311ba61d6efef5a17a002
gonshi.f <gonshi0606@i.softbank.jp>
2020-03-19 13:47:13 +0000

    kadai-tasklist fix

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -36,7 +36,7 @@ class TasksController extends Controller
         ]);
         
         $task = new Task;
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -72,7 +72,7 @@ class TasksController extends Controller
         ]);
         
         $task = Task::find($id);
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,18 +9,16 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>タイトル</th>
+                    <th>ステータス</th>
                     <th>メッセージ</th>
-                    <th>タスクリスト</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->title }}</td>
-                    <td>{{ $task->content }}</td>
                     <td>{{ $task->status }}</td>
+                    <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
